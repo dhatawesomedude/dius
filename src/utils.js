b7d08b5dfcfa5efbc4050b2d55e1f441912ce446
dhatawesomedude <deb2fast@gmail.com>
2017-03-20 21:36:08 +1100

    updated utils

--- src/utils.js
+++ src/utils.js
@@ -3,6 +3,7 @@
  */
 
 const config = require('./config.json');
+const product = require('./Product.js');
 
 const utils = () => {
     return {
@@ -16,8 +17,23 @@ const utils = () => {
             state.total_discount += discounted_amount;
             state.total_amount -= discounted_amount;
         },
-        products : config.products,
-        pricing_rules : config.pricing_rules
+        new_products: () => {
+            return {
+                new_i_pad: () => product(config.products.ipd.sku, config.products.ipd.name, config.products.ipd.price),
+
+                new_mac_book: () => product(config.products.mbp.sku, config.products.mbp.name, config.products.mbp.price),
+
+                new_apple_tv: () => product(config.products.atv.sku, config.products.atv.name, config.products.atv.price),
+
+                new_vga_adapter: () => product(config.products.vga.sku, config.products.vga.name, config.products.vga.price)
+            }
+        },
+        throw_if_no : p => {
+            throw new Error(`Missing parameter: ${p}`)
+        },
+        products: config.products,
+        pricing_rules: config.pricing_rules
     }
 };
+
 module.exports = utils;
